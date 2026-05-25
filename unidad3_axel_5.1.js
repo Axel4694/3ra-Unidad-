ced67c6d44fa700f5f311fa1a3896c313c8f422d
Axel <camposrojasaxelleonel9@gmail.com>
2026-05-24 23:04:25 -0600

    Add files via upload

---
+++ unidad3_axel_5.1.js
@@ -0,0 +1,4 @@
+for (let i = 1; i <= 5; i++) {
+    console.log(`Vuelta número: ${i}`);
+}
+let contador = 0;
---
+++ unidad3_axel_6.2.js
@@ -0,0 +1,5 @@
+let estudiantes = ["ana", "lui", "Maria", "pedro"];
+
+for (let i = 0; i < estudiantes.length; i++) {
+    console.log(`${i + 1}. ${estudiantes[i]}`);
+}
